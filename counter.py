6be926b10b3106613114534adb7ca5b47b93f7fb
Kim <kimberlyjpalmer.school@gmail.com>
2025-07-09 15:55:34 -0500

    passed

--- counter.py
+++ counter.py
@@ -1,8 +1,27 @@
 def countEmail():
     # This first line is provided for you
-    name = input("Enter file:")
+    name = input("Enter file: ")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
+    counts = {}
+    for line in handle:
+        if line.startswith("From "):
+            split_line = line.split()
+            sender = split_line[1]
+            counts[sender] = counts.get(sender, 0) + 1
+    
+    max_sender = 0
+    max_sent = 0
+    for email, number in counts.items():
+        if number > max_sent:
+            max_sender = email
+            max_sent = number
+    print(max_sender, max_sent)
+
+
+
+
+            
         
 
 ## if you want to test locally run > python counter.py
